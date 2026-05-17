d9900c79d29da1366443e33b55fb9918f0c22a62
Isaac <isaacbarreto011@gmail.com>
2026-05-17 14:01:58 -0300

    Add files via upload
    Adição do Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+function pegarValores(){
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  return [n1,n2];
+}
+function somar(){
+  let [n1,n2] = pegarValores();
+  alert("Resultado: " + (n1 + n2));
+}
+function subtrair(){
+  let [n1,n2] = pegarValores();
+  alert("Resultado:" + (n1 - n2));
+}
+function multiplicar(){
+  let [n1,n2] = pegarValores();
+  alert("Resultado: " + (n1 * n2));
+}
+function dividir(){
+  let [n1,n2] = pegarValores();
+  alert("Resultado: " + (n1 / n2));
+}
