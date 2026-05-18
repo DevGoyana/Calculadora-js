4f6bb0d35cb6a8263e93eee31326923d3391fd3a
Isaac <isaacbarreto011@gmail.com>
2026-05-17 22:04:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 function pegarValores(){
-  let n1 = Number(document.getElementById("num1").value);
-  let n2 = Number(document.getElementById("num2").value);
-  return [n1,n2];
+  let n1 = document.getElementById("num1").value;
+  let n2 = document.getElementById("num2").value;
+if (n1 === "" || n2 === "" ) {
+alert("Preencha os dois valores");
+return null;
 }
-function somar(){
-  let [n1,n2] = pegarValores();
-  alert("Resultado: " + (n1 + n2));
+return [Number(n1),Number(n2)];
 }
-function subtrair(){
-  let [n1,n2] = pegarValores();
-  alert("Resultado:" + (n1 - n2));
-}
-function multiplicar(){
-  let [n1,n2] = pegarValores();
-  alert("Resultado: " + (n1 * n2));
-}
-function dividir(){
-  let [n1,n2] = pegarValores();
-  alert("Resultado: " + (n1 / n2));
+function calcular(operacao){
+  let valores = pegarValores();
+  if (!valores) return;
+
+  let [n1, n2] = valores;
+  let resultado;
+
+  if (operacao === "soma") resultado = n1 + n2;
+  else if (operacao === "sub") resultado = n1 - n2;
+  else if (operacao === "mult") resultado = n1 * n2;
+  else if (operacao === "div") {
+   if (n2 === 0) {
+      alert("Não pode dividir por zero");
+      return;
+    }
+    resultado = n1 / n2;
+  }
+
+  document.getElementById("resultado").textContent = resultado;
+  
+  document.getElementById("num1").value = ""
+  document.getElementById("num2").value = ""
 }
